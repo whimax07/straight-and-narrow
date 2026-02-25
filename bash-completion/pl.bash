eef62cde9fb6ded61a61493595b0131118818bda
whimax07 <59751270+whimax07@users.noreply.github.com>
2026-02-25 20:22:41 +0000

    Fixed completion for pl.

--- bash-completion/pl.bash
+++ bash-completion/pl.bash
@@ -1,14 +1,13 @@
 
 function _pl_completion() {
     local current_word="${COMP_WORDS[COMP_CWORD]}"
-    local flags=("-l" "--less")
+    local containers=$(podman ps -a --format '{{.Names}}' | grep -vE '^[0-9a-fA-F]+-infra$')
+    local flags="-l"$'\n'"--less"$'\n'
     if [[ "$COMP_CWORD" == 1 ]]; then
-        local options=("${flags[@]}")
-        options+=$(podman ps -a --format '{{.Names}}' | grep -vE '^[0-9a-fA-F]+-infra$')
+        local options="$containers"$'\n'"$flags"
         COMPREPLY=( $(compgen -W "$options" -- "$current_word") )
-    elif [[ "$COMP_CWORD" == 2 ]] && [[ "$current_word" == "-l" || "$current_word" == "--less" ]]; then
-        local options=$(podman ps -a --format '{{.Names}}' | grep -vE '^[0-9a-fA-F]+-infra$')
-        COMPREPLY=( $(compgen -W "$options" -- "$current_word") )
+    elif [[ "$COMP_CWORD" == 2 ]] && [[ "${COMP_WORDS[1]}" == "-l" || "${COMP_WORDS[1]}" == "--less" ]]; then
+        COMPREPLY=( $(compgen -W "$containers" -- "$current_word") )
     fi
 }
 
